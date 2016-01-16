8f71f63ce0f3afe732e2ed68eedd952620343372
Andy Marks <amarks@thoughtworks.com>
2016-01-16 21:21:59 +1100

    Adding building blocks

--- src/alphabet_diamond/core.clj
+++ src/alphabet_diamond/core.clj
@@ -1,7 +1,20 @@
 (ns alphabet-diamond.core
-  (:gen-class))
+  	(:gen-class))
+
+(defn top-half [letter]
+	nil)
+	
+(defn half-height [letter]
+	(- 	(int (.charAt (clojure.string/upper-case letter) 0)) 
+		(- (int \A) 1)))
+
+(defmulti interrow-gap identity)
+(defmethod interrow-gap 1 [_] 0)
+(defmethod interrow-gap 2 [_] 1)
+(defmethod interrow-gap :default [row-number] 
+	(+ 2 (interrow-gap (- row-number 1))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  	"I don't do a whole lot ... yet."
+  	[& args]
+  	(println "Hello, World!"))
--- test/alphabet_diamond/midje.clj
+++ test/alphabet_diamond/midje.clj
@@ -1,5 +1,16 @@
-(ns alphabet_diamond.midje
+(ns alphabet-diamond.midje
+  (:require [alphabet-diamond.core :as ad])
   (:use [midje.sweet]))
 
-(fact (+ 2 2) => 5)
-(fact (+ 2 2) => odd?)
+(fact (ad/half-height "a") => 1)
+(fact (ad/half-height "A") => 1)
+(fact (ad/half-height "Z") => 26)
+(fact (ad/half-height "j") => 10)
+
+(fact (ad/interrow-gap 1) => 0)
+(fact (ad/interrow-gap 2) => 1)
+(fact (ad/interrow-gap 3) => 3)
+(fact (ad/interrow-gap 4) => 5)
+(fact (ad/interrow-gap 5) => 7)
+
+(fact (ad/top-half "A") => nil)
