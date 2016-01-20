17e88ab0f7924735172849e9c547c4b64fb4aa97
Andy Marks <amarks@thoughtworks.com>
2016-01-20 16:17:40 +1100

    Removing detritus from previous thinking

--- src/alphabet_diamond/core.clj
+++ src/alphabet_diamond/core.clj
@@ -1,37 +1,12 @@
 (ns alphabet-diamond.core
   	(:gen-class))
 
-(defn top-half [letter]
-	nil)
-
 (defn- letter-str->ucase-letter [letter]
 	(.charAt (clojure.string/upper-case letter) 0))
-
-(defn half-height [letter]
-	(- 	(int (letter-str->ucase-letter letter)) 
-		(- (int \A) 1)))
-
-(defmulti letter-gap identity)
-(defmethod letter-gap 1 [_] 0)
-(defmethod letter-gap 2 [_] 1)
-(defmethod letter-gap :default [row-number] 
-	(+ 2 (letter-gap (- row-number 1))))
 
 (defn letters-to [end-letter]
 	(map char (range (int \A) 
 		 (inc (int (letter-str->ucase-letter end-letter))))))
-
-(defn line-width [letter]
-	(if (= "A" letter)
-		1
-		(+ 2 (interrow-gap (half-height letter)))))
-
-(defn- top-row? [row-number]
-	(= row-number 1))
-
-(defn- gap [row-number]
-	(apply str 
-		(repeat (letter-gap row-number) " ")))
 
 (def ^:const row-template "ZYXWVUTSRQPONMLKJIHGFEDCBABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
--- test/alphabet_diamond/midje.clj
+++ test/alphabet_diamond/midje.clj
@@ -2,22 +2,6 @@
   (:require [alphabet-diamond.core :as ad])
   (:use [midje.sweet]))
 
-(fact (ad/half-height "a") => 1)
-(fact (ad/half-height "A") => 1)
-(fact (ad/half-height "Z") => 26)
-(fact (ad/half-height "j") => 10)
-
-(fact (ad/letter-gap 1) => 0)
-(fact (ad/letter-gap 2) => 1)
-(fact (ad/letter-gap 3) => 3)
-(fact (ad/letter-gap 4) => 5)
-(fact (ad/letter-gap 5) => 7)
-
-(fact (ad/line-width "A") => 1)
-(fact (ad/line-width "B") => 3)
-(fact (ad/line-width "C") => 5)
-(fact (ad/line-width "D") => 7)
-
 (fact (clojure.string/trim (ad/row 1 "A")) => "A")
 
 (fact (clojure.string/trim (ad/row 1 "B")) => "A")
@@ -29,6 +13,3 @@
 (fact (ad/letters-to "C") => [\A \B \C])
 (fact (ad/letters-to "A") => [\A])
 (fact (ad/letters-to "d") => [\A \B \C \D])
-
-(future-fact (ad/top-half "A") => nil)
-(future-fact (ad/top-half "B") => [" A "])
