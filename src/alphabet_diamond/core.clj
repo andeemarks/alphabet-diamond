d8ba04ae9bf4f6e85ae6fdd2192a88af2519eee7
Andy Marks <amarks@thoughtworks.com>
2016-01-23 20:10:27 +1100

    Cleanup

--- src/alphabet_diamond/core.clj
+++ src/alphabet_diamond/core.clj
@@ -1,24 +1,34 @@
 (ns alphabet-diamond.core
   	(:gen-class))
 
+(def ^:const row-template "ZYXWVUTSRQPONMLKJIHGFEDCBABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
 (defn- ucase-letter [letter]
 	(int (.charAt (clojure.string/upper-case letter) 0)))
 
-(defn- char-range [start end]
-  (map char (range (int start) (inc (int end)))))
+(defn- letter-range [start-letter end-letter]
+  (map char (range (int start-letter) (inc (int end-letter)))))
+
+(defn- instruction [index letter]
+	[(+ 1 index) (str (char letter))])
 
 (defn commands-for [end-letter]
-	(let [letters (map-indexed (fn [index letter] [(+ 1 index) (str (char letter))]) (char-range \A (ucase-letter end-letter)))]
-				(into (reverse letters) (rest (reverse letters)))))
-
-(def ^:const row-template "ZYXWVUTSRQPONMLKJIHGFEDCBABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	(let [letters (letter-range \A (ucase-letter end-letter))
+				rows-and-letters (reverse (map-indexed (fn [index letter] (instruction index letter)) letters))]
+				(into rows-and-letters (rest rows-and-letters))))
 
 (defn row [row-number letter]
 	(let [row-letter (nth (letters-to letter) (- row-number 1))]
-		(clojure.string/replace row-template (re-pattern (str "[^" row-letter "]")) " ")
+				(clojure.string/replace row-template (re-pattern (str "[^" row-letter "]")) " ")
 	))
+
+(defn diamond [letter]
+	(let [commands (commands-for letter)]
+		(map row commands)
+		)
+	)
 
 (defn -main
   	"I don't do a whole lot ... yet."
   	[& args]
-  	(println "Hello, World!"))
+  	(diamond args))
