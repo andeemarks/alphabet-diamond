d7e765dd94c46e139ab8f01e4eeb42b481f40fa6
Andy Marks <amarks@thoughtworks.com>
2016-01-24 21:46:17 +1100

    Refactor

--- src/alphabet_diamond/core.clj
+++ src/alphabet_diamond/core.clj
@@ -7,14 +7,15 @@
 	(int (.charAt (upper-case letter) 0)))
 
 (defn- letter-range [start-letter end-letter]
-  (map char (range (int start-letter) (inc (int end-letter)))))
+  (map char (range 	(int start-letter) 
+  									(inc (int end-letter)))))
 
 (defn- instruction [letter]
 	(str (char letter)))
 
 (defn row-instructions-for [end-letter]
 	(let [letters (letter-range \A (ucase-letter end-letter))
-				rows-and-letters (reverse (map (fn [letter] (instruction letter)) letters))]
+				rows-and-letters (reverse (map instruction letters))]
 			(into rows-and-letters (rest rows-and-letters))))
 
 (defn- everything-but [letter]
@@ -24,16 +25,14 @@
 	(str/replace row-template (everything-but letter) " "))
 
 (defn diamond [letter]
-	(let [instructions (row-instructions-for letter)]
-		(map row instructions)))
+	(map row (row-instructions-for letter)))
 
 (defn check-spec [spec]
 	{:pre [(not (nil? spec))
 				 (re-seq #"^[A-Za-z]$" (trim spec))]}
 		true)
 
-(defn -main
-  	[& args]
+(defn -main [& args]
   	(let [spec (first args)]
 	  	(try (check-spec spec)
 	  		(doall (map println (diamond spec)))
